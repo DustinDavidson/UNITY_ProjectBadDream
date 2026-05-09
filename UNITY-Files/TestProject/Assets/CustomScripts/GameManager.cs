2f30ba4b1c7aa89d6116e6de2106b1b630f23304
Dustin Davidson <davidsod@augsburg.edu>
2026-05-08 23:24:01 -0500

    Added TitleScreen

--- UNITY-Files/TestProject/Assets/CustomScripts/GameManager.cs
+++ UNITY-Files/TestProject/Assets/CustomScripts/GameManager.cs
@@ -1,10 +1,22 @@
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehavior
+public class GameManager : MonoBehaviour
 {
+
+    public void StartGame(){
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("GamePlay");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
     public void RestartGame()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
